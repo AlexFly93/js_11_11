8ccd511e1ed185debbcb96641d2248821d02cd49
AlexFly93 <muhabon93@gmail.com>
2022-11-11 10:12:54 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// function getTextAdultOrNot(value) {
+//   return value > 17 ? "adult" : "no adult";
+// }
+// console.log(getTextAdultOrNot());
+
+// const summa = 12345678;
+// const sale = summa > 10000 ? 20 :
+//              summa > 5000 ? 10 : 5;
